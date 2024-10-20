5443bc17db78549d2d0c798cae0aa23cc8368409
KatrinaZuus <katrinazuus@gmail.com>
2024-10-21 00:36:29 +0400

    fetch is done

--- src/Page/CostomersPage.tsx
+++ src/Page/CostomersPage.tsx
@@ -58,6 +58,7 @@ export default function CostomersPage(){
             });
             setSelectedRate(null); 
             SetIsdone(true)
+            console.log(isdone)
           } 
 
     return(
